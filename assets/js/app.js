4ca0d78fdd70b0795da5f064d5bf73d76befb92f
Ruslan <kotashov03@gmail.com>
2026-01-19 01:02:57 +0400

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', ()=>{
+    const bestSlider = new Swiper('.best__slider', {
+        slidesPerView: 4,
+        spaceBetween: 20,
+    })
+
+    const salesSlider = new Swiper('.sales__swiper', {
+        slidesPerView: 2.2,
+        spaceBetween: 20,
+    })
+})
